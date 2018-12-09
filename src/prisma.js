3cd74aecf9255d80797c6243252ecfe25fd60c43
Prajjwal <prajjwaldimri@hotmail.com>
2018-12-09 16:48:14 +0530

    Added cascade deletion relation to nodes

--- src/prisma.js
+++ src/prisma.js
@@ -5,38 +5,3 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466',
   secret: 'super-super',
 });
-
-const createBlogForUser = async (authorId, blogData) => {
-  const blog = await prisma.mutation.createBlog(
-    {
-      data: {
-        ...blogData,
-        author: {
-          connect: {
-            id: authorId,
-          },
-        },
-      },
-    },
-    '{ id }',
-  );
-  const user = await prisma.query.user(
-    {
-      where: { id: authorId },
-    },
-    '{ id name blogs { id, title, published } }',
-  );
-  return user;
-};
-
-createBlogForUser('cjpgl9uxb002o0710l3tmp26z', {
-  title: 'Forth Test Blog',
-  body: 'Lorem Ipsum Bald',
-  published: false,
-})
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
