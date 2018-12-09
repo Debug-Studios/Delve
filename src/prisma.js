92155d1b0558e45b1ab2f8e1b2a20215154bc2f8
Prajjwal <prajjwaldimri@hotmail.com>
2018-12-10 00:20:34 +0530

    Moved prisma secret

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,7 @@ import { Prisma } from 'prisma-binding';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'rogerdodgerbosssauce',
+  secret: process.env.PRISMA_SECRET,
 });
 
 export { prisma as default };
